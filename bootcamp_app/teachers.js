ee67e1d1cfb086978452d49c737612142497fcd6
Daniel Suen <danielsuen200791357@gmail.com>
2022-08-25 22:52:46 +0000

    formatted to prevent sql injection

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,15 +7,20 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+const queryString = `
 SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
 FROM teachers
 JOIN assistance_requests ON teachers.id = assistance_requests.teacher_id
 JOIN students ON students.id = assistance_requests.student_id
 JOIN cohorts ON cohorts.id = students.cohort_id
-WHERE cohorts.name = '${process.argv[2] || JUL02}'
+WHERE cohorts.name = $1
 ORDER BY teachers.name;
-`)
+`;
+const cohortName = process.argv[2]|| JUL02;
+// Store all potentially malicious values in an array.
+const values = [cohortName];
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.cohort}: ${user.teacher}`);
